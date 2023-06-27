e02f05fca50daa4924513b111bc33a7ac7badd9c
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-27 03:37:34 -0500

    cambiado la forma de acceder a las urls para independizar mejor las apps

--- telusko/urls.py
+++ telusko/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from apps.calc.views import home
+from django.urls import path,include
+
 urlpatterns = [
+    path('', include('apps.calc.urls')),
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
 
 ]
