56db05a61ed0092ab0ec2507269b38f684291824
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-28 23:48:50 -0500

    cambiado vistas en accounts

--- Tourist_places/apps/accounts/views.py
+++ Tourist_places/apps/accounts/views.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User, auth
 
 
 def login(request):
-    if request.method== 'POST':
+
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
@@ -17,7 +18,7 @@ def login(request):
             return redirect("/")
         else:
             messages.info(request,'invalid credentials')
-            return redirect('login')
+            return redirect('')
 
     else:
         return render(request,'login.html')    
@@ -35,19 +36,19 @@ def register(request):
         if password1==password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request,'Username Taken')
-                return redirect('register')
+                return redirect('register/')
             elif User.objects.filter(email=email).exists():
                 messages.info(request,'Email Taken')
-                return redirect('register')
+                return redirect('register/')
             else:   
                 user = User.objects.create_user(username=username, password=password1, email=email,first_name=first_name,last_name=last_name)
-                user.save();
+                user.save()
                 print('user created')
                 return redirect('login')
 
         else:
             messages.info(request,'password not matching..')    
-            return redirect('register')
+            return redirect('register/')
         return redirect('/')
         
     else:
