799dd9f61155d0ce86f654f0ec8de5776063da8b
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-29 01:45:17 -0500

    añadido y cambiado urls

--- Tourist_places/Tourist_places/urls.py
+++ Tourist_places/Tourist_places/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('apps.places.urls')),
-    path('',include('apps.accounts.urls')),
+    path('accounts/',include('apps.accounts.urls')),
     path('admin/', admin.site.urls),
 ]
 
