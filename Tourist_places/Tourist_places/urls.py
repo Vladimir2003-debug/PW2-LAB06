875d9cf299c6c900c3db9ae142f8ff9a1630bf5f
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-28 20:10:12 -0500

    añaido los urls de las apps

--- Tourist_places/Tourist_places/urls.py
+++ Tourist_places/Tourist_places/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-urlpatterns = [
 
+urlpatterns = [
+    path('travello',include('apps.travello.urls')),
+    path('',include('apps.accounts.urls')),
     path('admin/', admin.site.urls),
 ]
