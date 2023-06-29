6d80a46a2ab0815de47a1fb33fbe3138fec32f1f
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-28 19:49:00 -0500

    añadido el modelo destino turistico

--- Tourist_places/apps/places/models.py
+++ Tourist_places/apps/places/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.CharField(max_length=100)
+    imagenCiudad = models.ImageField()
+    precioTour = models.CharField(max_length=100)
+    ofertaTour = models.BooleanField(default=False)
